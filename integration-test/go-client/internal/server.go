d6562f035b0d8d37bc011a1be9ae15c86646d3b3
caangeenbrug-fvt <c.aangeenbrug@flikweertvision.nl>
2025-10-15 11:23:16 +0200

    gofmt -s -w ./..

--- integration-test/go-client/internal/server.go
+++ integration-test/go-client/internal/server.go
@@ -1,21 +1,21 @@
 package internal
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "OK")
+	fmt.Fprintf(w, "OK")
 }
 
 func StartServer() {
-    http.HandleFunc("/hello-world", helloWorldHandler)
-    http.HandleFunc("/health", healthCheckHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/hello-world", helloWorldHandler)
+	http.HandleFunc("/health", healthCheckHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
